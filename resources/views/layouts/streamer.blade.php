9f614e6b20720c33838993cb776ed2f0e3362462
Gustavo Vasquez <gustav.alfredo13@gmail.com>
2020-11-20 19:20:35 -0600

    barra de navegacion

--- resources/views/layouts/streamer.blade.php
+++ resources/views/layouts/streamer.blade.php
@@ -43,7 +43,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="#" class="brand-link">
             <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
@@ -68,42 +68,68 @@
                          with font-awesome or any other icon font library -->
 
                     <li class="nav-item">
-                        <a href="pages/widgets.html" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
-                                Widgets
+                                Inicio
+                                <span class="right badge badge-danger"></span>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-envelope"></i>
+                            <p>
+                                Mensajes
+                                <span class="right badge badge-danger"></span>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-barcode"></i>
+                            <p>
+                                Códigos
+                                <span class="right badge badge-danger"></span>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fab fa-superpowers"></i>
+                            <p>
+                                Ruleta
                                 <span class="right badge badge-danger">New</span>
                             </p>
                         </a>
                     </li>
-
-                    <li class="nav-header">EXAMPLES</li>
-
                     <li class="nav-item">
-                        <a href="pages/calendar.html" class="nav-link">
-                            <i class="nav-icon far fa-calendar-alt"></i>
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-rocket"></i>
                             <p>
-                                Calendar
-                                <span class="badge badge-info right">2</span>
+                                Votaciones
+                                <span class="right badge badge-danger">New</span>
                             </p>
                         </a>
-                        <a href="pages/calendar.html" class="nav-link">
-                            <i class="nav-icon far fa-calendar-alt"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-cog"></i>
                             <p>
-                                prueba
-                                <span class="badge badge-info right">2</span>
+                                Configuración
+                                <span class="right badge badge-danger"></span>
                             </p>
                         </a>
                     </li>
 
 
-                    <li class="nav-header">MISCELLANEOUS</li>
+                   <!--  <li class="nav-header">MISCELLANEOUS</li>
                     <li class="nav-item">
                         <a href="https://adminlte.io/docs/3.0" class="nav-link">
                             <i class="nav-icon fas fa-file"></i>
                             <p>Documentation</p>
                         </a>
-                    </li>
+                    </li> -->
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->
@@ -141,6 +167,7 @@
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
+    
     <footer class="main-footer">
         <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
         All rights reserved.
