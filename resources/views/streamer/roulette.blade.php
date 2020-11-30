906039e22eaac29a94ef314ec7b98c56e7d4d011
Hector Barillas <hector.interiano1925@gmail.com>
2020-11-29 21:34:53 -0600

    agregando dataTables a las vistas

--- resources/views/streamer/roulette.blade.php
+++ resources/views/streamer/roulette.blade.php
@@ -5,7 +5,7 @@
 @section('panel_actual')
     <i class="fab fa-superpowers mr-2 ml-2"></i>Panel de ruleta
 @endsection
-        
+
 @section('page_actual', 'Ruleta')
 
 @section('content')
@@ -18,39 +18,47 @@
             <div class="card mt-3">
                 <div class="card-header">
                     <h3 class="card-title">Ruletas</h3>
-                    <div class="card-tools">
-                        <div class="input-group input-group-sm hidden-xs" style="width: 150px;">
-                            <input style="height: inherit;" type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-                            <div class="input-group-btn">
-                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                            </div>
-                        </div>
-                    </div>
+{{--                    <div class="card-tools">--}}
+{{--                        <div class="input-group input-group-sm hidden-xs" style="width: 150px;">--}}
+{{--                            <input style="height: inherit;" type="text" name="table_search" class="form-control pull-right" placeholder="Search">--}}
+{{--                            <div class="input-group-btn">--}}
+{{--                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                 </div>
                 <div class="card-body table-responsive no-padding">
-                    <table class="table table-hover">
-                        <tbody>
+                    <table id="tabla_ruleta" class="table table-bordered table-striped table-hover">
+                        <thead>
                             <tr>
                                 <th>Recompensa</th>
                                 <th>Participantes</th>
                                 <th>Estado</th>
                                 <th>Acciones</th>
                             </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td>{{--<span class="label label-info">Activado</span>--}}</td>
-                                <td class="codes-actions">
-                                    {{--<i onclick="" class="fa ion-checkmark-round" title="Activar"></i>
-                                    <i onclick="" class="fa ion-close-round" title="Desactivar"></i>
-                                    <i onclick="" class="fa ion-trash-a" title="Eliminar"></i>
-                                    <a href="" target="_blank" title="Ir a la ruleta"><i class="fa ion-trophy"></i></a>--}}
-                                </td>
-                            </tr>
-                        </tbody>
+                        </thead>
                     </table>
                 </div>
             </div>
         </section>
     </div>
+@endsection
+@section('scripts')
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#tabla_ruleta').DataTable({
+                {{--processing: true,--}}
+                {{--serverSide:true--}}
+                {{--ajax: "{!! route('streamer.getcodigos') !!}",--}}
+                {{--columns: [--}}
+                {{--    { data: 'id', name: 'id' },--}}
+                {{--    { data: 'name', name: 'name' },--}}
+                {{--    { data: 'email', name: 'email' },--}}
+                {{--    { data: 'email', name: 'email' },--}}
+                {{--    { data: 'email', name: 'email' },--}}
+                {{--    { data: 'email', name: 'email' },--}}
+                {{--]--}}
+            });
+        });
+    </script>
 @endsection
