f14e083b8897ec1d77c0587c422e32a2db74f5c4
Ricardo Sifontes <jrsifontes16@gmail.com>
2020-12-10 23:38:43 -0600

    Adding prefix to web routes

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware('auth')->group(function () {
                 $view = '/streamer';
                 break;
             case 3:
-                $view = '/admin/dashboard';
+                $view = '/dashboard';
                 break;
             default:
                 $view = '/user';
@@ -47,69 +47,67 @@ Route::middleware('auth')->group(function () {
     });
 
     /* ---- SUPER USER ---- */
+    Route::prefix('admin')->group(function () {
+        Route::get('/dashboard', [AdminConfigurationController::class,'index'])->name('adminDashboard');
+        Route::get('/usuarios', [AdminConfigurationController::class,'showUsers'])->name('users');
+        Route::get('/getUsers', [AdminConfigurationController::class,'getUsers'])->name('getUsers');
+        Route::post('/registerAdmin', [AdminConfigurationController::class,'createAdmin'])->name('registerAdmin');
+        Route::get('/configuraciones', [AdminConfigurationController::class,'showConfigs'])->name('configs');
 
-    Route::get('/admin/dashboard', [AdminConfigurationController::class,'index'])->name('adminDashboard');
-
-    Route::get('/admin/usuarios', [AdminConfigurationController::class,'showUsers'])->name('users');
-    Route::get('/admin/getUsers', [AdminConfigurationController::class,'getUsers'])->name('getUsers');
-    Route::post('/admin/registerAdmin', [AdminConfigurationController::class,'createAdmin'])->name('registerAdmin');
-    Route::get('/admin/configuraciones', [AdminConfigurationController::class,'showConfigs'])->name('configs');
-
-    Route::get('/admin/social_networks', [SocialNetworkController::class,'index'])->name('social_networks');
-    Route::post('/admin/social_networks/add', [SocialNetworkController::class,'add']);
+        Route::get('/social_networks', [SocialNetworkController::class,'index'])->name('social_networks');
+        Route::post('/social_networks/add', [SocialNetworkController::class,'add']);
 
 
-    Route::post('/admin/configuraciones/roulette', [AdminConfigurationController::class,'editRoulette']);
-    Route::post('/admin/configuraciones/codes', [AdminConfigurationController::class,'editCodes']);
-    Route::post('/admin/configuraciones/polls', [AdminConfigurationController::class,'editPolls']);
-    Route::post('/admin/configuraciones/messages', [AdminConfigurationController::class,'editMessages']);
+        Route::post('/configuraciones/roulette', [AdminConfigurationController::class,'editRoulette']);
+        Route::post('/configuraciones/codes', [AdminConfigurationController::class,'editCodes']);
+        Route::post('/configuraciones/polls', [AdminConfigurationController::class,'editPolls']);
+        Route::post('/configuraciones/messages', [AdminConfigurationController::class,'editMessages']);
+    });
+
 
     /* ---- / SUPER USER ---- */
 
     /* ---- STREAMER ---- */
+    Route::prefix('streamer')->group(function () {
+        Route::get('/', [StreamerController::class,'index']);
 
-    Route::get('/streamer', [StreamerController::class,'index']);
+        // modulo de configuracion para streamer
+        Route::get('/config', [ConfiguracionController::class, 'index']);
 
-    // modulo de configuracion para streamer
-    Route::get('/streamer/config', [ConfiguracionController::class, 'index']);
+        // modulo de codigos para streamer
+        Route::get('/codigos', [CodigoController::class, 'index'])->name('streamer.codigos');
+        Route::post('/getcodigos', [CodigoController::class, 'get_datos'])->name('streamer.getcodigos');
+        Route::post('/nuevocodigo', [CodigoController::class, 'crear'])->name('streamer.nuevocodigo');
+        Route::post('/activarcodigo', [CodigoController::class, 'activar'])->name('streamer.activarcodigo');
+        Route::post('/desactivarcodigo', [CodigoController::class, 'desactivar'])->name('streamer.desactivarcodigo');
 
-    // modulo de codigos para streamer
-	Route::get('/streamer/codigos', [CodigoController::class, 'index'])->name('streamer.codigos');
-	Route::post('/streamer/getcodigos', [CodigoController::class, 'get_datos'])->name('streamer.getcodigos');
-	Route::post('/streamer/nuevocodigo', [CodigoController::class, 'crear'])->name('streamer.nuevocodigo');
-	Route::post('/streamer/activarcodigo', [CodigoController::class, 'activar'])->name('streamer.activarcodigo');
-	Route::post('/streamer/desactivarcodigo', [CodigoController::class, 'desactivar'])->name('streamer.desactivarcodigo');
+        Route::get('/message', [MessageController::class, 'index']);
+        Route::get('/roulette', [RouletteController::class, 'index'])->name('streamer.roulette');
+        Route::get('/roulette/getroulette', [RouletteController::class, 'get_roulettes'])->name('streamer.getroulette');
+        Route::post('/roulette/create_roulette', [RouletteController::class, 'createRoulette'])->name('streamer.roulette.createroulette');
+        Route::get('/votaciones', [VotacionesController::class, 'index'])->name('streamer.votaciones');
+        Route::get('/getvotaciones', [VotacionesController::class, 'get_votaciones'])->name('streamer.getvotaciones');
+        Route::post('/votaciones/create_poll', [VotacionesController::class, 'createPoll'])->name('streamer.votaciones.createpoll');
 
-    Route::get('/streamer/message', [MessageController::class, 'index']);
-    Route::get('/streamer/roulette', [RouletteController::class, 'index'])->name('streamer.roulette');
-    Route::get('/streamer/roulette/getroulette', [RouletteController::class, 'get_roulettes'])->name('streamer.getroulette');
-    Route::post('/streamer/roulette/create_roulette', [RouletteController::class, 'createRoulette'])->name('streamer.roulette.createroulette');
-    Route::get('/streamer/votaciones', [VotacionesController::class, 'index'])->name('streamer.votaciones');
-    Route::get('/streamer/getvotaciones', [VotacionesController::class, 'get_votaciones'])->name('streamer.getvotaciones');
-    Route::post('/streamer/votaciones/create_poll', [VotacionesController::class, 'createPoll'])->name('streamer.votaciones.createpoll');
-
-    Route::get('/streamer/spin_roulette', function (){
-        return view('streamer.spin_roulette');
+        Route::get('/spin_roulette', function (){
+            return view('streamer.spin_roulette');
+        });
     });
 
     /* ---- /STREAMER ---- */
 
     /* ---- USER ---- */
 
-    Route::get('/user', [ViewerController::class,'index']);
+    Route::prefix('user')->group(function () {
+        Route::get('/', [ViewerController::class,'index']);
+    });
+
 
     /* ---- / USER ---- */
 
 });
 
 
-/* ---- / AUTH ROUTES ---- */
-
-// modulo de codigos para streamer
-// Route::get('/streamer/codigos', [CodigoController::class, 'index'])->name('streamer.codigos');
-// Route::get('/streamer/getcodigos', [CodigoController::class, 'get_datos'])->name('streamer.getcodigos');
-
-/* ---- /STREAMER ---- */
 /* ---- PUBLIC ROUTES ---- */
 
 
